0bcdfbf4b89225d6967ce13c9818226d2b37c28a
arturoluna879 <arturoluna879@gmail.com>
2021-04-01 13:24:33 -0400

    mejora responsive

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -57,7 +57,7 @@
                                 @endforeach
                                 </tbody>
                             </table>
-                            <div class="w-75 mx-auto mt-4 alert alert-info d-flex justify-content-between"> 
+                            <div class="w-100 mx-auto mt-4 alert alert-info d-flex justify-content-between"> 
                                 <p class="m-0">Total Monto Restante</p>
                                 <h5 class="m-0">{{$total_pending}}</h5>
                             </div>
