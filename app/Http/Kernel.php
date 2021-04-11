0d8074d88c353ad6be59cb98ba095483b36961ee
arturoluna879 <arturoluna879@gmail.com>
2021-04-10 21:28:08 -0400

    CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware.
-     *
+     * 
      * These middleware may be assigned to groups or used individually.
      *
      * @var array
