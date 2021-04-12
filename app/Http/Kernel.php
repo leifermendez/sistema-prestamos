030b0a894fa118670e5de6701dcb7d5e03864ff4
arturoluna879 <arturoluna879@gmail.com>
2021-04-12 17:50:54 -0400

    antes de pull mleifer

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     /**
-     * The application's global HTTP middleware stack.
+     * The  application's global HTTP middleware stack.
      *
      * These middleware are run during every request to your application.
      *
