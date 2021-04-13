0b8f9220c443f19b1ad751d0c1a4b9efdb0231e0
arturoluna879 <arturoluna879@gmail.com>
2021-04-12 23:00:13 -0400

    prueba

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware.
      * 
-     * These middleware may be assigned to groups or used individually.
+     * These  middleware may be assigned to groups or used individually.
      *
      * @var array
      */
